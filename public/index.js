3501bc22910e6e4be0ffddbc0eac488e44730498
DJ Asuncion <deorabbinjibe777@gmail.com>
2020-03-01 14:27:29 +0800

    16/16

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 d3.select("body")
   .append("h1")
   .attr("id", "title")
-  .text("Scatterplot Graph");
+  .text("Doping in Professional Bicycle Racing");
 
 const margin = {
     top: 100,
@@ -26,6 +26,7 @@ const tooltip = d3
   .select("body")
   .append("div")
   .attr("class", "tooltip")
+  .attr("id", "tooltip")
   .style("opacity", 0);
 
 const svg = d3
@@ -99,7 +100,48 @@ d3.json(
       .attr("cy", d => y(d.Time))
       .attr("data-xvalue", d => d.Year)
       .attr("data-yvalue", d => d.Time.toISOString())
-      .style("fill", d => color(d.Doping != ""));
+      .style("fill", d => color(d.Doping != ""))
+      .on("mouseover", d => {
+        tooltip
+          .style("opacity", 0.9)
+          .attr("data-year", d.Year)
+          .html(
+            `${d.Name}: ${d.Nationality} <br/> Year: ${
+              d.Year
+            }, Time: ${timeFormat(d.Time)} <br /> <br /> ${d.Doping}`
+          )
+          .style("left", `${d3.event.pageX}px`)
+          .style("top", `${d3.event.pageY - 29}px`);
+      })
+      .on("mouseout", d => {
+        tooltip.style("opacity", 0);
+      });
+
+    const legend = svg
+      .selectAll(".legend")
+      .data(color.domain())
+      .enter()
+      .append("g")
+      .attr("class", "legend")
+      .attr("id", "legend")
+      .attr("transform", (d, i) => `translate(0, ${height / 2 - i * 20})`);
+
+    legend
+      .append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+    legend
+      .append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(d =>
+        d ? "Riders with doping allegations" : "No doping allegations"
+      );
   })
   .catch(error => {
     console.error(error);
